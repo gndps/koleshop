c5a06bbd62554c78568887ad8da521e2f75b1fae
Gundeep Singh <gndp@live.com>
2015-03-20 02:02:07 +0530

    Basic product info fragment added

---
+++ app/src/main/java/com/gndps/kolshopmaterial/fragments/BasicProductInfoShopkeeper.java
@@ -0,0 +1,34 @@
+package com.gndps.kolshopmaterial.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gndps.kolshopmaterial.R;
+
+public class BasicProductInfoShopkeeper extends Fragment {
+
+    public BasicProductInfoShopkeeper() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_basic_product_info_shopkeeper, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
