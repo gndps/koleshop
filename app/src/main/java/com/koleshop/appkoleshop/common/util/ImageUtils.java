3693255769a080b811fd90bc54322db7945f13dd
Gundeep Singh <gndp@live.com>
2015-12-09 22:25:14 +0530

    added some shit

---
+++ app/src/main/java/com/koleshop/appkoleshop/common/util/ImageUtils.java
@@ -0,0 +1,7 @@
+package com.koleshop.appkoleshop.common.util;
+
+/**
+ * Created by Gundeep on 06/12/15.
+ */
+public class ImageUtils {
+}
---
+++ app/src/main/java/com/koleshop/appkoleshop/model/parcel/EditProductVar.java
@@ -0,0 +1,7 @@
+package com.koleshop.appkoleshop.model.parcel;
+
+/**
+ * Created by Gundeep on 07/12/15.
+ */
+public class EditProductVar {
+}
